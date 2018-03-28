ff4ea3ac84dfedf6b7f3c55991126a6c06bea533
Vishal Sharma <vsh1819@gmail.com>
2018-03-28 17:11:11 +0100

    created routes and server.js, replacing next dev in package.json with node server.js

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,8 @@ export default props => {
   return (
     <Container>
       <Head>
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-    </Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+      </Head>
       <Header/>
       {props.children}
 
