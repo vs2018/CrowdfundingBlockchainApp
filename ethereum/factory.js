5866efd41320c5d70b6b57799e8315e1d74bf347
Vishal Sharma <vsh1819@gmail.com>
2018-03-28 08:24:28 +0100

    created instance of campaignfactory

---
+++ ethereum/factory.js
@@ -0,0 +1,11 @@
+import web3 from './web3'
+
+import CampaignFactory from './build/CampaignFactory.json'
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  '0x13283B793e980860567E8cf44B37B1e89154fBBd',
+
+)
+
+export default instance
